7522092246ed2c6300e7dcbef1af82a14bbe1127
Tom van Bolderen <tvanbolderen@gmail.com>
2022-05-26 13:31:14 +0200

    Updated generate-helm-docs and READMEs

--- tools/generate-helm-docs.sh
+++ tools/generate-helm-docs.sh
@@ -13,13 +13,15 @@ command -v docker >/dev/null 2>&1 || {
 # Absolute path of repository
 repository=$(git rev-parse --show-toplevel)
 
+docker pull jnorwood/helm-docs:latest
+
 for train in charts
 do
   for chart in ${train}/*; do
     if [ -d "${chart}" ]; then
       chartname=$(basename ${chart})
       chartversion=$(cat ${chart}/Chart.yaml | grep "^version: " | awk -F" " '{ print $2 }')
-      
+
       docker container run \
         --rm \
         --volume "$(pwd):/helm-docs" \
